2acb4cd9dff174b71e95d1f9e2a84872c4a1a98d
Yusuf-At <137978149+Yusuf-At@users.noreply.github.com>
2023-09-05 10:11:28 +0700

    Add new function 'game', adding feature to the previous function

--- Rock_paper_scissor.js
+++ Rock_paper_scissor.js
@@ -4,6 +4,9 @@ const choice = [
     "scissor"
 ]
 
+playerScore = 0
+compScore = 0
+
 function getComputerChoice() {
     randomIndex = Math.floor(Math.random() * choice.length);
     return choice[randomIndex];
@@ -14,26 +17,40 @@ function plySelection() {
     return plyselection.toLowerCase();
 }
 
-const playerSelection = plySelection();
-const computerSelection = getComputerChoice();
-
-console.log(`com = ${computerSelection}`)
-console.log(`ply = ${playerSelection}`)
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === "paper" && playerSelection === "rock" ) {
+        compScore += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (computerSelection === "scissor" && playerSelection === "paper" ) {
+        compScore += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (computerSelection === "rock" && playerSelection === "scissor" ) {
+        compScore += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (computerSelection === playerSelection) {
         return `Draw! ${computerSelection} cannot beat ${playerSelection}`;
-    } else if (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissor") {
+    } else if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissor") {
         return `Try again, You enter ${playerSelection}`;
     } else {
+        playerScore += 1;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
   }
 
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    playerSelection = plySelection();
+    computerSelection = getComputerChoice();
+    console.log(`computer = ${computerSelection}`)
+    console.log(`player = ${playerSelection}`)
+
+    console.log(playRound(playerSelection, computerSelection)) 
+    console.log(`Your score = ${playerScore}`)
+    console.log(`Computer score = ${compScore}`)
+}
+
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
